008ae70b1ef505abe8d55375d83e3963b3441a2e
pavani2244 <pavanipedineedi24@gmail.com>
2024-09-29 21:04:06 +0100

    commit changes

--- euler.js
+++ euler.js
@@ -2,7 +2,8 @@ a=3;
 b=5;
 n=1000;
 
-function sumofMultiplesofThreeandFiveLessThanAThousand(){
+function sumofMultiplesofThreeandFiveLessThanAThousand()
+{
     let sum = 0;
     for ( let i = 0; i<1000; i++){
         if (i%3 == 0 || i%5 ==0){
@@ -14,7 +15,8 @@ function sumofMultiplesofThreeandFiveLessThanAThousand(){
 
 }
 
-function sumofMultiples(a,b,n){
+function sumofMultiples(a,b,n)
+{
     let sum = 0;
     for (let i = 1; i < n; i++) {
         if (i % a === 0 || i % b === 0) {
@@ -24,7 +26,8 @@ function sumofMultiples(a,b,n){
     return sum;
 }
 
-function calculate(){
+function calculate()
+{
     const a = parseInt(document.getElementaryById('a').value);
     const b = parseInt(document.getElementaryById('b').value);
     const n = parseInt(document.getElementaryById('n').value);
