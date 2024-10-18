5fb92328bcd23a5c67cc869de307ef9fd371593b
pavani2244 <pavanipedineedi24@gmail.com>
2024-10-18 14:06:29 +0100

    hdn

--- basket.js
+++ basket.js
@@ -1,12 +1,12 @@
 let f1 =  (basket,prices)=> {
     let totalcost = 0;
     for(let item in basket){
-        if(prices(item] !  undefined){
+        if(prices[item] !=  undefined){
             totalcost  +=basket[item] * prices[item];
         }
     }
     return totalcost;
 }
-basket = (apple:1,oranges:,grapes:5);
-price=(apple:2.50,oranges: 3.50,grapes:5.0,tea:10,milk:50);
+basket = {apple:1,oranges:3,grapes:5};
+price={apple:2.50,oranges: 3.50,grapes:5.0,tea:10,milk:50};
 alert(f1(basket,price));
