dd97e04ed63dde97fb036b7583e49b66d0e509a0
pavani2244 <pavanipedineedi24@gmail.com>
2024-10-18 13:32:23 +0100

    commit changes

---
+++ basket.js
@@ -0,0 +1,12 @@
+let f1 =  (basket,prices)=> {
+    let totalcost = 0;
+    for(let item in basket){
+        if(prices[item] ! = undefined){
+            totalcost  +=basket[item] * prices[item];
+        }
+    }
+    return totalcost;
+}
+basket = (apple:1,oranges:,grapes:5);
+price=(apple:2.50,oranges: 3.50,grapes:5.0,tea:10,milk:50);
+alert(f1(basket,price));
